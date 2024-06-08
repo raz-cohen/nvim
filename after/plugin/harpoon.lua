8ecbbb2144a30a87789c49ad094c7ea19da53d9b
Raz Cohen <razcohen16@gmail.com>
2024-06-08 22:11:50 +0300

    feat: Changing harpoon prev & next keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -13,5 +13,5 @@ vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:lis
 --vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-h>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-k>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-j>", function() harpoon:list():next() end)
