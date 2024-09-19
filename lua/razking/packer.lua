0d23066cc4760f8d697a4d8cc0d0946b52000bd6
RAZ COHEN <razcohen@cellcom.co.il>
2024-09-19 10:38:27 +0300

    feat: remove nvim-treesitter configuration
    The nvim-treesitter configuration has been removed from the project.
    This includes the deletion of the treesitter.lua file and the removal
    of the nvim-treesitter plugin from packer.lua. This change simplifies
    the setup and reduces dependencies.

--- lua/razking/packer.lua
+++ lua/razking/packer.lua
@@ -23,8 +23,6 @@ return require('packer').startup(function(use)
     })
 
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-
     use "nvim-lua/plenary.nvim"
     use {
         "ThePrimeagen/harpoon",
