8abe2fc7f61178f59d079cf213efb4108ccf3e13
Raz Cohen <razcohen16@gmail.com>
2024-09-22 22:14:47 +0300

    chore: lsp handle lua_ls ls, with default suggested config.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,25 +1,60 @@
 local lsp_zero = require("lsp-zero")
 local mason = require("mason")
 local masonLSP = require("mason-lspconfig")
+local lspconfig = require("lspconfig")
 
 mason.setup({})
 masonLSP.setup({
-	ensure_installed = {
-		'tsserver',
-		'eslint',
-	},
-	handlers = {
-		function(server_name)
-			require("lspconfig")[server_name].setup({})
-		end
-	}
+    ensure_installed = {
+        'eslint',
+        'lua_ls',
+    },
+    handlers = {
+        function(server_name)
+            lspconfig[server_name].setup({})
+        end
+    }
 })
+
+lspconfig.lua_ls.setup {
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+                return
+            end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                -- Tell the language server which version of Lua you're using
+                -- (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME,
+                    -- Depending on the usage, you might want to add additional paths here.
+                    "${3rd}/luv/library",
+                    -- "${3rd}/busted/library",
+                }
+                -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                -- library = vim.api.nvim_get_runtime_file("", true)
+            }
+        })
+    end,
+    settings = {
+        Lua = {}
+    }
+}
 
 
 lsp_zero.preset("recommended")
 
 lsp_zero.set_preferences({
-	sign_icons = {}
+    sign_icons = {}
 })
 
 
